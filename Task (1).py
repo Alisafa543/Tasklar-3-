a61ea0435645a0cac861eb15509e1b5a267eed6f
Əlisəfa Pirhesenov <elisefapirhesenov@gmail.com>
2024-11-26 15:44:18 +0400

    Tasklar 3

---
+++ Task (1).py
@@ -0,0 +1,4 @@
+my_list = ["python" , "java" , "go" , "php" , "c++"]
+secim = input("Yeni metni daxil edin: ")
+my_list[2] = secim
+print(my_list)
---
+++ Task (2).py
@@ -0,0 +1,7 @@
+my_list = ["apple" , "cherry" , "orange" , "banana"]
+a = my_list[-1] 
+ilkherf  = a[0]
+if ilkherf == "b":
+    print("Son element b herfi ile baslayir.")
+else :
+    print("Son element b herfi ile baslamir.")
